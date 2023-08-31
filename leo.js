faa03764776a3302bbe9da3f8627b7e4081f970d
leonardo123452 <124888323+leonardo123452@users.noreply.github.com>
2023-08-31 09:20:26 -0300

    Add files via upload

---
+++ leo.js
@@ -0,0 +1,12 @@
+Tempo = 5000;
+const idade = 18;
+age = 18;
+age = age + 1
+var timer =setInterval(chamar,Tempo)
+
+function chamar(){
+    //document.getElement so e chamado com tempo
+document.getElementById("mostrar").innerHTML = idade;
+document.getElementById("exibir").innerHTML = age;
+
+}
